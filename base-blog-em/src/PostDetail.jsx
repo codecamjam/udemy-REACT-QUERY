43cbcbe4ae446faaff0de6641f60edfcce7a9b96
codecamjam <camerontaghlabi@gmail.com>
2022-06-02 21:15:47 -0400

    l16: mutation to update post title

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -34,6 +34,8 @@ export function PostDetail({ post }) {
   //can actually take an argument itself
   const deleteMutation = useMutation((postId) => deletePost(postId));
 
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
   if (isLoading) {
     return <h3>Loading!</h3>;
   }
@@ -51,6 +53,11 @@ export function PostDetail({ post }) {
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
       <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+
       {deleteMutation.isError && (
         <p style={{ color: 'red' }}>Error deleting the post</p>
       )}
@@ -60,7 +67,16 @@ export function PostDetail({ post }) {
       {deleteMutation.isSuccess && (
         <p style={{ color: 'green' }}>Post has (not) been deleted</p>
       )}
-      <button>Update title</button>
+      {/*****************************************/}
+      {updateMutation.isError && (
+        <p style={{ color: 'red' }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (

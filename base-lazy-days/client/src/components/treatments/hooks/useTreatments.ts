12744560acac9d7e0519c91e2d05132ba56b1fd5
codecamjam <camerontaghlabi@gmail.com>
2022-06-05 15:28:12 -0400

    l34: onError Handler for useQuery

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,7 +12,15 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
+
   const fallback = [];
-  const { data = [] } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = [] } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error ? error.message : 'error connecting to server';
+      toast({ title, status: 'error' });
+    },
+  });
   return data;
 }

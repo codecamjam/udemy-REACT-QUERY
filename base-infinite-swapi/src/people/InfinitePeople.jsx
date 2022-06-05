eb77faf2ab824efb65954ba255bf79f326d20a3f
codecamjam <camerontaghlabi@gmail.com>
2022-06-05 13:05:17 -0400

    l23: infiniteScroll component

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -17,5 +17,18 @@ export function InfinitePeople() {
     }
   );
 
-  return <InfiniteScroll />;
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData) =>
+        pageData.results.map((person) => (
+          <Person
+            key={person.name}
+            name={person.name}
+            hairColor={person.hair_color}
+            eyeColor={person.eye_color}
+          />
+        ))
+      )}
+    </InfiniteScroll>
+  );
 }
